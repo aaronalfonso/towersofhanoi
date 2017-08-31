3cf576f029dcddfccbf100b01c3e287d0cb8a228
Aaron Alfonso <31370853+aaronalfonso@users.noreply.github.com>
2017-08-30 23:24:17 -0400

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,44 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print(":)");
+        Scanner keyboard = new Scanner(System.in);
+        int currentNumber = 1;
+        int numberOfSteps = 6;
 
-    }
-}
+
+        System.out.println("Enter how many times you want the look-and-say sequence to refresh");
+        numberOfSteps = keyboard.nextInt();
+        while (numberOfSteps != 0) {
+
+            numberOfSteps = numberOfSteps - 1;
+            if (currentNumber == 1) {
+                System.out.println(currentNumber);
+                currentNumber = 11;
+                System.out.println(currentNumber);
+            } else if (currentNumber == 11) {
+                currentNumber = 21;
+                System.out.println(currentNumber);
+            } else if (currentNumber == 21) {
+                currentNumber = 1211;
+                System.out.println(currentNumber);
+            } else if (currentNumber == 1211) {
+                currentNumber = 111221;
+                System.out.println(currentNumber);
+            } else if (currentNumber == 111221) {
+                currentNumber = 312211;
+                System.out.println(currentNumber);
+            } else if (currentNumber == 312211) {
+                currentNumber = 13112211;
+                System.out.println(currentNumber);
+            } else if (currentNumber == 13112211) {
+                currentNumber = 1113212221;
+                System.out.println(currentNumber);
+            }
+        }
+        System.out.println("Sequence Finished.");
+        }
+        }
