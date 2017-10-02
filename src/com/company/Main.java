fe51e86fdedc5c7d7db1eba167469b345274c079
Aaron Alfonso <31370853+aaronalfonso@users.noreply.github.com>
2017-10-01 20:28:52 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,44 +1,44 @@
 package com.company;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Scanner keyboard = new Scanner(System.in);
-        int currentNumber = 1;
-        int numberOfSteps = 6;
+        Scanner userInput = new Scanner(System.in);
+        String input = userInput.nextLine();
+        int length = input.length();
+        double randomPlace = Math.random() * 5;
+        double randomLoca = Math.random() * length;
+        int place = (int)randomPlace;
+        int count = 0;
+        int randomLoc = (int)randomLoca;
+        int loc = randomLoc;
+        char[] finalString = input.toCharArray();
 
+        for (count=0; count <= length; count++) {
+            if (place == 0) {
+                finalString[loc] = Integer.toHexString(finalString[loc]); //Trying to find this method for string/char instead of Integer
+                count++;
+                place = 1;
+            } else if (place == 1) {
+                finalString[loc] = Integer.toBinaryString(); //Trying to find this method for string/char instead of Integer
+                count++;
+                place = 2;
+            } else if (place == 2) {
+                count++;
+                place = 3;
+            } else if (place == 3) {
+                count++;
+                place = 4;
+            } else if (place == 4) {
+                count++;
+                place = 0;
+            }
+            if (randomLoc > length) {
 
-        System.out.println("Enter how many times you want the look-and-say sequence to refresh");
-        numberOfSteps = keyboard.nextInt();
-        while (numberOfSteps != 0) {
-
-            numberOfSteps = numberOfSteps - 1;
-            if (currentNumber == 1) {
-                System.out.println(currentNumber);
-                currentNumber = 11;
-                System.out.println(currentNumber);
-            } else if (currentNumber == 11) {
-                currentNumber = 21;
-                System.out.println(currentNumber);
-            } else if (currentNumber == 21) {
-                currentNumber = 1211;
-                System.out.println(currentNumber);
-            } else if (currentNumber == 1211) {
-                currentNumber = 111221;
-                System.out.println(currentNumber);
-            } else if (currentNumber == 111221) {
-                currentNumber = 312211;
-                System.out.println(currentNumber);
-            } else if (currentNumber == 312211) {
-                currentNumber = 13112211;
-                System.out.println(currentNumber);
-            } else if (currentNumber == 13112211) {
-                currentNumber = 1113212221;
-                System.out.println(currentNumber);
             }
         }
-        System.out.println("Sequence Finished.");
-        }
-        }
+        System.out.println(finalString[0]);
+    }
+}
