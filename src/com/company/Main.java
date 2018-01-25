939081f9afaa22fd819f6ef780bac381c5046c8a
Aaron Alfonso <31370853+aaronalfonso@users.noreply.github.com>
2018-01-24 21:48:11 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,15 @@
 package com.company;
 
 import java.util.*;
-import java.lang.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int exit = 2;
-        while (exit == 2){
-            Scanner userInput = new Scanner(System.in);
-            System.out.println("Hi, what's your name?");
-            String name = userInput.nextLine();
-            exit = 1;
-            while (exit == 1){
-                perfectRandom randomNum = new perfectRandom();
-                int randomNumber = randomNum.perfectRandom();
-                System.out.println("Hi " + name + ". Your lucky number for today is " + randomNumber);
-                System.out.println("If you would like to exit, enter 0. To get another lucky number, enter 1. To change your name, enter 2.");
-                exit = userInput.nextInt();
-            }
-            if (exit == 0) {
-                System.out.println("Exiting program...");
-
-            }
-            if (exit != 0 && exit != 1 && exit != 2) {
-                System.out.println("Incorrect integer entered, exiting program...");
-            }
-        }
-
+        Scanner userInput = new Scanner(System.in);
+	    
+	    System.out.println("Enter the amount of disks in the game");
+	    int num = userInput.nextInt();
+	    
     }
 }
